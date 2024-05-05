7a2acd2777b0c8ca40d8bdc760d4407da2ec818d
Yuki <miyake_yuuki320@yahoo.co.jp>
2024-05-05 15:00:58 +0900

    グラフ3つ完成

--- app.py
+++ app.py
@@ -78,7 +78,59 @@ def upload_csv():
             margin=dict(l=20, r=20, t=40, b=40),
         )
 
-        return render_template('index.html', plot=fig.to_html(include_plotlyjs='cdn'))
+        # 　二つ目
+        # 上位n人の発明者を表示する
+        n = 20
+        inventor_application_counts_sorted = inventor_application_counts.sort_values(
+            ascending=False)
+
+        top_inventors = inventor_application_counts_sorted.index[:n]
+        top_counts = inventor_application_counts_sorted.values[:n]
+
+        # データフレームを作成
+        df = pd.DataFrame({
+            "発明者": top_inventors[::-1],
+            "出願件数": top_counts[::-1]
+        })
+
+        # 棒グラフを作成
+        fig2 = px.bar(df, x="出願件数", y="発明者", orientation="h",
+                      labels={"発明者": "発明者/権利者", "出願件数": "出願件数"})
+        # # y軸のメモリを100ごとに表示
+        # fig.update_layout(yaxis=dict(showticklabels=True))
+        # # 数字のフォントサイズを大きくする
+        # fig.update_traces(textfont_size=46)
+        fig2.update_layout(width=900, height=700)
+
+        # fig3
+        # 不明なキーを削除
+        top_inventor_counts = inventor_counts.drop(index=inventor_counts.index[~inventor_counts.index.isin(
+            top_inventors)], inplace=False)
+
+        top_inventors_list = top_inventors.tolist()
+        reversed_top_inventors_list = top_inventors_list[::-1]
+        # top_inventor_counts = top_inventor_counts.loc[reversed_top_inventors_list]
+        top_inventor_counts = top_inventor_counts.loc[top_inventors_list]
+
+        # 発明者/権利者名のリストを作成
+        top_inventor_names = top_inventor_counts.columns.tolist()
+
+        # パテントマップを作成
+        fig3 = px.imshow(top_inventor_counts, x=top_inventor_names,
+                         y=top_inventor_counts.index,  # y軸の値を指定
+                         labels={"x": "年", "y": "発明者/権利者", "color": "出願件数"},
+                         title="発明者/権利者ごとの一年ごとの出願数のパテントマップ")
+        # x軸の目盛を1年ごとに設定
+        # dtickで目盛間隔を1年、tickformatで年のみ表示
+        fig3.update_xaxes(dtick=1, tickformat="%Y")
+
+        # fig.update_yaxes(tickwidth=1)
+        # fig.update_xaxes(tickwidth=1)
+        fig3.update_layout(width=1500, height=600)
+
+        return render_template('index.html', plot=fig.to_html(include_plotlyjs='cdn'),
+                               plot2=fig2.to_html(include_plotlyjs='cdn'),
+                               plot3=fig3.to_html(include_plotlyjs='cdn'))
 
     else:
         return '有効なCSVファイルをアップロードしてください。'
